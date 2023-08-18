fcddcc9a505cc77d81ba2294de0d170e394ff0d3
hassan channa <tanhapanchi484@gmail.com>
2023-08-18 21:48:21 +0500

    Js
    1St assig

---
+++ app.js
@@ -0,0 +1,180 @@
+//Q1 ....1//
+
+
+//   alert('AsslaamAllakium Sir');
+
+///...2.../
+// alert('Error! Please enter valid password');
+///.3///
+
+// alert("Well come to Js world \n Happy Coding");
+//...4..///
+//   alert('Well come to Js land...');
+
+//   alert('Happy Coding!');
+
+//5//
+// console.log("Hello....I can run JS through message browser  developer console");
+
+
+
+
+
+
+
+
+
+
+
+
+//Chapter 2//
+//1 2 1. Declare a variable called username.
+//2. Declare a variable called myName & assign to it a string 
+//that represents your Full Name.//
+
+// var username = "Gul"
+// var myName = "Hassan"
+// alert(username +myName)
+
+//3//
+
+
+// var titledmessage = "Hello World"
+// alert(titledmessage)
+
+//4/
+
+// var studentName = "Jhone Doe"
+// alert(studentName);
+// var studentage = "18"
+// alert(studentage);
+// var course = "Web And Appliction development"
+// alert(course);
+
+//5//
+
+
+// var food = ("PIZZA \n PIZZ \n PIZ \n PI \n P ")
+
+// alert(food);
+
+//6//
+// var emailText = "My Email Addres is ";
+// var email = ("Gulhassan143@gmail.com")
+// alert(emailText+email)
+//7//
+
+// var book = "I am trying to learn from the book A SMART \n Way to learn Javascript"
+// alert(book)
+//8//
+// document.getElementsByTagName(fristText)
+// //9//
+// var text = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert(text)
+
+// chapter 2 complete
+
+// chapter 3
+
+
+//1////
+
+// var age = " I am 19 Years Old"
+// alert(age)
+//2//
+// var   prvisted = prompt("Enter Your Number")
+// var visted = prvisted;
+// visted = ("U have visted ")
+// alert(visted+prvisted) 
+
+
+
+// var age = prompt("Enter Your Birth Year")
+// var yearOfBirth = 
+// age ("Your Birth year is ")
+// console.log(age+yearOfBirth)
+
+
+// De
+// var birthYear = 2004;
+
+// // Display a message 
+// document.write("Your birth year is " + birthYear);
+
+// Store visitor's 
+
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+
+// // Display the message in the browser
+// var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store";
+// document.write(message);
+
+
+// var variable1, variable2, variable3;
+
+//     
+//         var myVariable;
+//         var _underscore;
+//         var $dollarSign;
+//         var camelCase;
+//         var _123Numbers;
+
+
+//         // Display the message in the HTML
+//         document.write("a) Variable names can only contain letters, numbers, underscores, and dollar signs.<br>");
+//         document.write("b) Variables must begin with a letter, underscore, or dollar sign. For example: $name, _name or name<br>");
+//         document.write("c) Variable names are case-sensitive.<br>");
+//         document.write("d) Variable names should not be JavaScript reserved keywords.<br>");
+
+// chapter 05
+
+
+//1
+
+// var num1 = 3;
+// var num2 = 5;
+
+// var addition = num1 + num2;
+// var subtraction = num1 - num2;
+// var multiplication = num1 * num2;
+// var division = num1 / num2;
+// var modulus = num1 % num2;
+
+// document.write("Addition: " + addition + "<br>");
+// document.write("Subtraction: " + subtraction + "<br>");
+// document.write("Multiplication: " + multiplication + "<br>");
+// document.write("Division: " + division + "<br>");
+
+//3...........
+ // Task 3: Mathematical 
+//  var value; 
+
+//  document.write("Value after variable declaration is: " + value + "<br>"); // b
+
+//  value = 5; // c
+
+//  document.write("Initial value: " + value + "<br>"); // d
+
+//  value++; // e
+
+//  document.write("Value after increment is: " + value + "<br>"); // f
+
+//  value += 7; // g
+
+//  document.write("Value after addition is: " + value + "<br>"); // h
+
+//  value--; // i
+
+//  document.write("Value after decrement is: " + value + "<br>"); 
+
+
+
+// var number = 4;
+        
+// // Display the multiplication table in the browser
+// document.write("<h2>Multiplication Table of " + number + "</h2>");
+// for (var i = 1; i <= 10; i++) {
+//     var result = number * i;
+//     document.write(number + " × " + i + " = " + result + "<br>");}
